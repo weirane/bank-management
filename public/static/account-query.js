1fe264d5ba7dde1c6f4fca5a86da7fb83d66dd9e
Wang Ruochen <wangkevin920@163.com>
2020-06-23 15:01:43 +0800

    Check account type at the beginning

--- public/static/account-query.js
+++ public/static/account-query.js
@@ -1,17 +1,21 @@
 const acctype = document.querySelector('#query-account select');
 const save = document.getElementById('type-save');
 const check = document.getElementById('type-check');
+const checkAccountType = () => {
+    if (acctype.value === '') {
+        save.classList.add('hide');
+        check.classList.add('hide');
+    } else if (acctype.value === '0') {
+        save.classList.remove('hide');
+        check.classList.add('hide');
+    } else if (acctype.value === '1') {
+        save.classList.add('hide');
+        check.classList.remove('hide');
+    }
+};
+
 for (const types of acctype.getElementsByTagName('option')) {
-    types.addEventListener('click', (_) => {
-        if (acctype.value === '') {
-            save.classList.add('hide');
-            check.classList.add('hide');
-        } else if (acctype.value === '0') {
-            save.classList.remove('hide');
-            check.classList.add('hide');
-        } else if (acctype.value === '1') {
-            save.classList.add('hide');
-            check.classList.remove('hide');
-        }
-    });
+    types.addEventListener('click', checkAccountType);
 }
+
+checkAccountType();
