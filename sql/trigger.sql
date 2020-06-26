0a4b1dea37c54719f518e34593605b4d4157af3e
Wang Ruochen <wangkevin920@163.com>
2020-06-26 10:07:40 +0800

    Shorter SQL

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -51,16 +51,12 @@ for each row begin
     if t is null then
         signal sqlstate '45001' set message_text = '账户不存在';
     end if;
-    if t = 0 then
-        select count(account_id) into n from account right join has_account using(account_id)
-        where customer_id = new.customer_id and account.bank = b and type = 0;
-        if n != 0 then
+    select count(account_id) into n from account right join has_account using(account_id)
+    where customer_id = new.customer_id and account.bank = b and type = t;
+    if n != 0 then
+        if t = 0 then
             signal sqlstate '45000' set message_text = '储蓄账户已存在';
-        end if;
-    elseif t = 1 then
-        select count(account_id) into n from account right join has_account using(account_id)
-        where customer_id = new.customer_id and account.bank = b and type = 1;
-        if n != 0 then
+        elseif t = 1 then
             signal sqlstate '45000' set message_text = '支票账户已存在';
         end if;
     end if;
