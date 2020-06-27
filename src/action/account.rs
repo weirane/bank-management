f291572cd929c33af03f0ae2ce0f0316e6adde3b
Wang Ruochen <wangkevin920@163.com>
2020-06-27 22:19:41 +0800

    Enable searching for accounts using customer

--- src/action/account.rs
+++ src/action/account.rs
@@ -44,50 +44,114 @@ pub async fn change_check(id: &str, key: &str, val: &str, pool: &MySqlPool) -> R
 
 pub async fn query_save(form: &crate::route::SMap, pool: &MySqlPool) -> Result<Vec<SaveAccount>> {
     let empty = String::new();
-    sqlx::query_as!(
-        SaveAccount,
-        "select account_id as id, bank, balance,
-            substr('储蓄账户支票账户', type*4+1, 4) as type_,
-            open_date, interest_rate, currency from saveaccounts where
-        account_id like concat('%', ?, '%')
-        and bank like concat('%', ?, '%')
-        and balance like concat('%', ?, '%')
-        and open_date like concat('%', ?, '%')
-        and interest_rate like concat('%', ?, '%')
-        and currency like concat('%', ?, '%')
-        ",
-        form.get("id").unwrap_or(&empty),
-        form.get("bank").unwrap_or(&empty),
-        form.get("balance").unwrap_or(&empty),
-        form.get("open_date").unwrap_or(&empty),
-        form.get("interest_rate").unwrap_or(&empty),
-        form.get("currency").unwrap_or(&empty),
-    )
-    .fetch_all(pool)
-    .await
-    .map_err(Into::into)
+    let customer = form.get("customer").unwrap_or(&empty);
+    if !customer.is_empty() {
+        sqlx::query_as!(
+            SaveAccount,
+            "select distinct account_id as id, bank, balance,
+                substr('储蓄账户支票账户', type*4+1, 4) as type_,
+                open_date, interest_rate, currency
+            from (
+                select account_id, customer_id
+                from has_account left join customer
+                using(customer_real_id)
+            ) x right join saveaccounts using(account_id) where
+            customer_id like concat('%', ?, '%')
+            and account_id like concat('%', ?, '%')
+            and bank like concat('%', ?, '%')
+            and balance like concat('%', ?, '%')
+            and open_date like concat('%', ?, '%')
+            and interest_rate like concat('%', ?, '%')
+            and currency like concat('%', ?, '%')
+            ",
+            customer,
+            form.get("id").unwrap_or(&empty),
+            form.get("bank").unwrap_or(&empty),
+            form.get("balance").unwrap_or(&empty),
+            form.get("open_date").unwrap_or(&empty),
+            form.get("interest_rate").unwrap_or(&empty),
+            form.get("currency").unwrap_or(&empty),
+        )
+        .fetch_all(pool)
+        .await
+        .map_err(Into::into)
+    } else {
+        sqlx::query_as!(
+            SaveAccount,
+            "select account_id as id, bank, balance,
+                substr('储蓄账户支票账户', type*4+1, 4) as type_,
+                open_date, interest_rate, currency from saveaccounts where
+            account_id like concat('%', ?, '%')
+            and bank like concat('%', ?, '%')
+            and balance like concat('%', ?, '%')
+            and open_date like concat('%', ?, '%')
+            and interest_rate like concat('%', ?, '%')
+            and currency like concat('%', ?, '%')
+            ",
+            form.get("id").unwrap_or(&empty),
+            form.get("bank").unwrap_or(&empty),
+            form.get("balance").unwrap_or(&empty),
+            form.get("open_date").unwrap_or(&empty),
+            form.get("interest_rate").unwrap_or(&empty),
+            form.get("currency").unwrap_or(&empty),
+        )
+        .fetch_all(pool)
+        .await
+        .map_err(Into::into)
+    }
 }
 
 pub async fn query_check(form: &crate::route::SMap, pool: &MySqlPool) -> Result<Vec<CheckAccount>> {
     let empty = String::new();
-    sqlx::query_as!(
-        CheckAccount,
-        "select account_id as id, bank, balance,
-            substr('储蓄账户支票账户', type*4+1, 4) as type_,
-            open_date, credit from checkaccounts where
-        account_id like concat('%', ?, '%')
-        and bank like concat('%', ?, '%')
-        and balance like concat('%', ?, '%')
-        and open_date like concat('%', ?, '%')
-        and credit like concat('%', ?, '%')
-        ",
-        form.get("id").unwrap_or(&empty),
-        form.get("bank").unwrap_or(&empty),
-        form.get("balance").unwrap_or(&empty),
-        form.get("open_date").unwrap_or(&empty),
-        form.get("credit").unwrap_or(&empty),
-    )
-    .fetch_all(pool)
-    .await
-    .map_err(Into::into)
+    let customer = form.get("customer").unwrap_or(&empty);
+    if !customer.is_empty() {
+        sqlx::query_as!(
+            CheckAccount,
+            "select account_id as id, bank, balance,
+                substr('储蓄账户支票账户', type*4+1, 4) as type_,
+                open_date, credit
+            from (
+                select account_id, customer_id
+                from has_account left join customer
+                using(customer_real_id)
+            ) x right join checkaccounts using(account_id) where
+            customer_id like concat('%', ?, '%')
+            and account_id like concat('%', ?, '%')
+            and bank like concat('%', ?, '%')
+            and balance like concat('%', ?, '%')
+            and open_date like concat('%', ?, '%')
+            and credit like concat('%', ?, '%')
+            ",
+            customer,
+            form.get("id").unwrap_or(&empty),
+            form.get("bank").unwrap_or(&empty),
+            form.get("balance").unwrap_or(&empty),
+            form.get("open_date").unwrap_or(&empty),
+            form.get("credit").unwrap_or(&empty),
+        )
+        .fetch_all(pool)
+        .await
+        .map_err(Into::into)
+    } else {
+        sqlx::query_as!(
+            CheckAccount,
+            "select account_id as id, bank, balance,
+                substr('储蓄账户支票账户', type*4+1, 4) as type_,
+                open_date, credit from checkaccounts where
+            account_id like concat('%', ?, '%')
+            and bank like concat('%', ?, '%')
+            and balance like concat('%', ?, '%')
+            and open_date like concat('%', ?, '%')
+            and credit like concat('%', ?, '%')
+            ",
+            form.get("id").unwrap_or(&empty),
+            form.get("bank").unwrap_or(&empty),
+            form.get("balance").unwrap_or(&empty),
+            form.get("open_date").unwrap_or(&empty),
+            form.get("credit").unwrap_or(&empty),
+        )
+        .fetch_all(pool)
+        .await
+        .map_err(Into::into)
+    }
 }
